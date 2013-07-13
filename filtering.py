cd76c62cfc2387be2306d2d27c677f9416318b54
haya14busa <hayabusa1419@gmail.com>
2013-07-13 20:57:38 +0900

    filtering.py added

---
+++ filtering.py
@@ -0,0 +1,16 @@
+# p22
+
+import string
+
+allchars = string.maketrans('', '')
+def makefilter(keep):
+    delchars = allchars.translate(allchars, keep)
+
+    def thefilter(s):
+        return s.translate(allchars, delchars)
+    return thefilter
+
+if __name__ == '__main__':
+    just_vowels = makefilter('aeiouy')
+    print just_vowels('four score and seven years ago')
+    print just_vowels('tiger, tiger burning bright')
