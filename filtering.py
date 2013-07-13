a75092db22bab6c75d6ef096633aa08129ba23b9
haya14busa <hayabusa1419@gmail.com>
2013-07-13 21:04:20 +0900

    canonicforms added into filtering.py

--- filtering.py
+++ filtering.py
@@ -10,7 +10,12 @@ def makefilter(keep):
         return s.translate(allchars, delchars)
     return thefilter
 
+
+def canonicform(s):
+    return makefilter(s)(allchars)
+
 if __name__ == '__main__':
     just_vowels = makefilter('aeiouy')
     print just_vowels('four score and seven years ago')
     print just_vowels('tiger, tiger burning bright')
+    print canonicform('hayabusa')
