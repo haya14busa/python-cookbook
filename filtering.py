2eb94e292831fe36b32e9b91899df35d9712ea89
haya14busa <hayabusa1419@gmail.com>
2013-07-13 22:39:07 +0900

    understand canonicform

--- filtering.py
+++ filtering.py
@@ -12,10 +12,14 @@ def makefilter(keep):
 
 
 def canonicform(s):
-    return makefilter(s)(allchars)
+    return makefilter(s)(allchars) #s->keep, allchars->filterd letters
 
 if __name__ == '__main__':
     just_vowels = makefilter('aeiouy')
     print just_vowels('four score and seven years ago')
     print just_vowels('tiger, tiger burning bright')
+
     print canonicform('hayabusa')
+    # test = makefilter('hayabusa')(allchars)
+    # print test
+    # print just_vowels(allchars)
